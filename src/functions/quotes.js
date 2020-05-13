4d1dfcdbc223b2f9928a808ea1166d7b69fe7c31
Michael Sprague <mikesprague@gmail.com>
2020-05-13 15:27:42 -0400

    :recycle: Normalize quote data server-side

--- src/functions/quotes.js
+++ src/functions/quotes.js
@@ -6,10 +6,38 @@ exports.handler = async (event, context, callback) => {
     'Access-Control-Allow-Headers': 'Content-Type',
   };
 
+  const normalizeQuoteData = (apiData) => {
+    const returnData = apiData.map((quoteData) => {
+      const {
+        content,
+        excerpt,
+        link: quoteLink,
+        title,
+      } = quoteData || null;
+      const {
+        rendered: quoteExcerpt,
+      } = excerpt;
+      const {
+        rendered: quoteHtml,
+      } = content;
+      const {
+        rendered: quoteAuthor,
+      } = title;
+
+      return {
+        quoteExcerpt,
+        quoteHtml,
+        quoteAuthor,
+        quoteLink,
+      };
+    });
+    return returnData;
+  };
+
   const quotesData = await axios.get('https://quotesondesign.com/wp-json/wp/v2/posts/?orderby=rand')
     .then((response) => {
       // console.log(response);
-      return response.data;
+      return normalizeQuoteData(response.data);
     }).catch((error) => {
       console.error(error);
       return {
